7f5b551c89bc835b217a5b0c72472b08adda8d4a
Kovács Ferenc <ferenc_kovacs@epam.com>
2020-04-15 15:01:32 +0200

    replace legacy components with funcional components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-export default () => (
-  <div className="App">
-    <h1>HW</h1>
-    <p>this is really working wtf</p>
-    <Person />
-  </div>
-);
+export default () =>{
+  const [showPersons, setShowPersons] = useState(false); 
+  const [persons, setPersons] = useState([
+    { id: '11', name: 'Max', age: 28 },
+    { id: '22', name: 'Manu', age: 29 },
+    { id: '33', name: 'Stephanie', age: 26 }
+  ]);
+  let renderedPersons = null;
 
-// return React.createElement('div', {className: 'App'}, React.createElement('h1',null, 'HW'));
+  const nameChangedHandler = ( event, id ) => {
+    const personIndex = persons.findIndex(p => p.id === id);
+    const personsCopy = [...persons];
+    const person = { ...persons[personIndex]};
+    person.name = event.target.value;
+    personsCopy[personIndex] = person;
+    setPersons(personsCopy);
+  }
+
+  const deletePersonHandler = (personIndex) => {
+    const personsCopy = [...persons];
+    personsCopy.splice(personIndex, 1);
+    setPersons(personsCopy);
+  }
+
+  const togglePersonsHandler = () => setShowPersons(!showPersons);
+
+  const style = {
+    backgroundColor: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px',
+    cursor: 'pointer'
+  };
+
+  if (showPersons) {
+    renderedPersons = (
+      <div>
+        {persons.map((person, index) => {
+          return <Person
+            click={() => deletePersonHandler(index)}
+            name={person.name} 
+            age={person.age}
+            key={person.id}
+            changed={(event) => nameChangedHandler(event, person.id)} />
+        })}
+      </div>
+    );
+
+    style.backgroundColor = 'green';
+  }
+
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <button style={style} onClick={togglePersonsHandler}>Toggle Persons</button>
+      {renderedPersons}
+    </div>
+  );
+}
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,3 +1,8 @@
 import React from 'react';
+import './Person.css';
 
-export default () => <p>I'm a person!</p>
+export default (props) => 
+<div className="Person">
+  <p onClick={props.click}>I'm {props.name}</p>
+  <input type="text" onChange={props.changed} value={props.name} />
+</div> 
